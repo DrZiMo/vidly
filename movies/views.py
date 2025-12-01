e1c3fc09e546eef5f969e74e93e8007a3a6ec5a9
DrZiMo <zuhaybhamar@gmail.com>
2025-12-01 06:11:49 +0300

    feat: Implement initial movie listing and detail views with associated templates and URL configuration.

--- movies/views.py
+++ movies/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .models import Movie
 
@@ -8,3 +8,8 @@ from .models import Movie
 def index(request):
     movies = Movie.objects.all()
     return render(request, 'movies/index.html', {'movies': movies})
+
+
+def detail(request, movie_id):
+    movie = get_object_or_404(Movie, id=movie_id)
+    return render(request, 'movies/detail.html', {'movie': movie})
